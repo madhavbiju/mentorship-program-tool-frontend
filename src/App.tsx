88c5e12a2fa4c0f4eecc83c2eb391cc4e345605e
Madhav Biju <65170463+madhavbiju@users.noreply.github.com>
2024-01-23 12:51:45 +0530

    Initial Push

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { CssVarsProvider } from '@mui/joy/styles';
+import CssBaseline from '@mui/joy/CssBaseline';
+import Box from '@mui/joy/Box';
+import Sidebar from './components/Sidebar';
+import Header from './components/Header';
+import AdminDashboard from './components/Pages/AdminDashboard';
 
-function App() {
+export default function JoyOrderDashboardTemplate() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <CssVarsProvider disableTransitionOnChange>
+      <CssBaseline />
+      <Box sx={{ display: 'flex',flexDirection: 'column', minHeight: '100dvh' }}>
+        <Header />
+        <Sidebar />
+        <Box
+          component="main"
+          className="MainContent"
+          sx={{
+            pl: { xs: 2, md: '240px' },
+            pr: {xs:2, md:2},
+            pt: {
+              xs: 'calc(12px + var(--Header-height))',
+              sm: 'calc(12px + var(--Header-height))',
+              md: 'calc(12px + var(--Header-height))',
+            },
+            pb: { xs: 2, sm: 2, md: 3 },
+            flex: 1,
+            display: 'flex',
+            flexDirection: 'column',
+            minWidth: 0,
+            height: '100dvh',
+            gap: 1,
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          
+           {/* Here */}
+          <AdminDashboard/>
+            </Box>
+      </Box>
+    </CssVarsProvider>
   );
 }
-
-export default App;
