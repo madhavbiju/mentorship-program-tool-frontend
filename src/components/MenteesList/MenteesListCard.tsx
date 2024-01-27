4343169df65d1fd0dabc283eff24f4615252786a
Mehanoor Basheer <mehanoorbasheer7@gmail.com>
2024-01-27 17:21:39 +0530

    feat:mentor-menteeList display

---
+++ src/components/MenteesList/MenteesListCard.tsx
@@ -0,0 +1,62 @@
+import { People } from "@mui/icons-material";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemDecorator,
+  Avatar,
+  ListDivider,
+  Typography,
+  Grid,
+  Stack,
+} from "@mui/joy";
+import React from "react";
+
+const MenteesListCard = () => {
+  return (
+    <Box>
+      <Box sx={{ marginRight: "10%" }}>
+        <List
+          orientation="horizontal"
+          variant="soft"
+          sx={{
+            flexGrow: 0,
+            mx: "auto",
+            "--ListItemDecorator-size": "48px",
+            "--ListItem-paddingY": "1rem",
+            borderRadius: "sm",
+            py: "2%",
+            justifyContent: "center",
+          }}
+        >
+          <ListItem sx={{ mr: 12 }}>
+            <ListItemDecorator>
+              <Avatar src="/static/images/avatar/1.jpg" />
+            </ListItemDecorator>
+          </ListItem>
+          
+          <Box sx={{ display: "flex" }}>
+          <ListDivider inset="gutter" />
+            <Stack>
+              <Typography sx={{ml:9}} style={{ fontSize: "10px"}}>Mentee</Typography>
+              <ListItem sx={{ mx: 5 }}>Boyd Burt</ListItem> 
+            </Stack>
+            
+            <ListDivider inset="gutter" />
+            <Stack>
+            <Typography sx={{ml:9}} style={{ fontSize: "10px"}}>Program</Typography>
+            <ListItem sx={{ mx: 4 }}>Program Name</ListItem>
+            </Stack>
+            <ListDivider inset="gutter" />
+            <Stack>
+            <Typography sx={{ml:9}} style={{ fontSize: "10px"}}>End date</Typography>
+            <ListItem sx={{ ml: 4 }}>12/02/2024</ListItem>
+            </Stack>
+          </Box>
+        </List>
+      </Box>
+    </Box>
+  );
+};
+
+export default MenteesListCard;
---
+++ src/pages/mentor/menteesList/MenteesList.tsx
@@ -0,0 +1,37 @@
+import { Box, Button, Dropdown, Menu, MenuButton, MenuItem, MenuList } from "@mui/joy";
+import React from "react";
+import MenteesListCard from "../../../components/MenteesList/MenteesListCard";
+
+const MenteesList = () => {
+  return (
+    <Box>
+      <Box sx={{ display: "flex", ml: 3, mt: 3 }}>
+        <Dropdown>
+          <MenuButton color="primary" sx={{borderRadius:10 ,px:"3%"}}>Sort</MenuButton>
+          <Menu>
+            <MenuItem>Choose item</MenuItem>
+            <MenuList>Add item</MenuList>
+            <MenuList>Add item</MenuList>
+          </Menu>
+        </Dropdown>
+        <Dropdown>
+          <MenuButton color="primary" sx={{borderRadius:10 ,mx:"3%",px:"3%"}}>Filter</MenuButton>
+          <Menu>
+            <MenuItem>Choose item</MenuItem>
+            <MenuList>Add item</MenuList>
+            <MenuList>Add item</MenuList>
+          </Menu>
+        </Dropdown>
+      </Box>
+      <Box sx={{my:"3%", mx:"2%"}} >
+        <MenteesListCard  />
+        <br />
+        <MenteesListCard/>
+        <br/>
+        <MenteesListCard/>
+      </Box>
+    </Box>
+  );
+};
+
+export default MenteesList;
