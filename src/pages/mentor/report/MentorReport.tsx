daff71c2e4e9cd895a51463c48c05208ff37b505
Shiyas Ahamed S <shiyas.s@experionglobal.com>
2024-02-01 12:09:22 +0530

    fix\merging mentor and mentee report

--- src/pages/mentor/report/MentorReport.tsx
+++ src/pages/mentor/report/MentorReport.tsx
@@ -1,86 +1,106 @@
-import { Button, Grid, Typography } from "@mui/joy";
-import { Box, flexbox } from "@mui/system";
+import { Box, Typography } from "@mui/joy";
+import { Grid, Button } from "@mui/joy";
 import React from "react";
 import MentorMenteeTable from "../../../components/MentorMenteeTable/MentorMenteeTable";
-import MentorReportPieChart from "../../../components/PieChart/MentorReportPieChart";
+import ReportMeetingsTable from "../../../components/ReportTables/ReportMeetingsTable";
 import ReportTaskTable from "../../../components/ReportTables/ReportTaskTable";
-import MeetingsTable from "../../../components/ReportTables/ReportMeetingsTable";
-import { end } from "@popperjs/core";
-import BandHighlight from "../../../components/PieChart/MentorReportPieChart";
+import SelectMenteeDropDown from "../../../components/SelectMenteeDropDown/SelectMenteeDropDown";
 import SortByMentorMenteeTable from "../../../components/Sort/SortByMentorMenteeTable";
+import SortByReportMeetingsTable from "../../../components/Sort/SortByReportMeetingsTable";
+import SortByReportTaskTable from "../../../components/Sort/SortByReportTaskTable";
+import MentorReportGraph from "../../../components/MentorReportGraph/MentorReportGraph";
 
 const MentorReport = () => {
   return (
     <div>
       <Box>
-        <Grid container sx={{ display: "flex", alignItems: "center" }}>
-          <Grid xs={3}>
+        <Grid container sx={{ display: "flex", justifyContent: "flex-end" }}>
+          <Grid xs={12} lg={9}>
             <Typography level={"h2"}>Report</Typography>
           </Grid>
 
-          <Grid xs={3} md={3}>
+          <Grid xs={12} lg={3}>
             <Button size="md">Download</Button>
           </Grid>
         </Grid>
+        <Grid container sx={{ display: "flex", alignItems: "center" }}>
+          <Grid lg={6}>
+            <Grid
+              xs={12}
+              lg={6}
+              columnGap={1}
+              container
+              sx={{ display: "flex" }}
+            >
+              <Grid>
+                <Typography level={"h3"}>Pairs</Typography>
+              </Grid>
 
-        <Grid container md={8}>
-          <Grid
-            container
-            xs={12}
-            md={6}
-            sx={{ display: "flex", justifyContent: "space-between" }}
-          >
-            <Grid>
-              <Typography level={"h3"}>Pairs</Typography>
+              <Grid>
+                <SortByMentorMenteeTable />
+              </Grid>
             </Grid>
-            <Grid>
-              <SortByMentorMenteeTable />
-            </Grid>
-          </Grid>
-          <Grid container>
-            <Grid xs={12} md={7}>
+
+            <Grid xs={12} lg={6}>
               <MentorMenteeTable />
             </Grid>
           </Grid>
-
-          <Grid xs={5} md={5}>
-            <MentorReportPieChart />
+          <Grid lg={6}>
+            <MentorReportGraph />
           </Grid>
         </Grid>
+        <br />
 
         <Grid container>
+          <Grid
+            xs={12}
+            lg={3}
+            container
+            sx={{ display: "flex", justifyContent: "space-between" }}
+          >
+            <Grid>
+              <Typography level={"h3"}>Tasks</Typography>
+            </Grid>
+
+            <Grid>
+              <SortByReportTaskTable />
+            </Grid>
+            <Grid>
+              <SelectMenteeDropDown />
+            </Grid>
+          </Grid>
+
           <Grid>
-            <Typography level={"h3"}>Tasks</Typography>
-          </Grid>
-          <Grid container>
-            <Grid></Grid>
-
-            <Grid></Grid>
-          </Grid>
-
-          <Grid container>
-            <Grid xs={12} md={8}>
+            <Grid xs={12} lg={11}>
               <ReportTaskTable />
             </Grid>
-            <Grid></Grid>
           </Grid>
         </Grid>
+        <br />
 
         <Grid container>
-          <Grid>
-            <Typography level={"h3"}>Meetings</Typography>
-          </Grid>
-          <Grid container>
-            <Grid></Grid>
-
-            <Grid></Grid>
-          </Grid>
-
-          <Grid container>
-            <Grid xs={12} md={8}>
-              <MeetingsTable />
+          <Grid
+            xs={12}
+            lg={4}
+            container
+            sx={{ display: "flex", justifyContent: "space-between" }}
+          >
+            <Grid>
+              <Typography level={"h3"}>Meetings</Typography>
             </Grid>
-            <Grid></Grid>
+
+            <Grid>
+              <SortByReportMeetingsTable />
+            </Grid>
+            <Grid>
+              <SelectMenteeDropDown />
+            </Grid>
+          </Grid>
+
+          <Grid>
+            <Grid xs={12} lg={11}>
+              <ReportMeetingsTable />
+            </Grid>
           </Grid>
         </Grid>
       </Box>
