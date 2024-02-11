031ef35557b1ebe166730a7b9eba4094b1931db9
Madhav Biju <65170463+madhavbiju@users.noreply.github.com>
2024-02-11 12:30:38 +0530

    feat: Add post for Meeting

--- src/pages/mentor/createMeeting/CreateMeetingHandler.tsx
+++ src/pages/mentor/createMeeting/CreateMeetingHandler.tsx
@@ -1,19 +1,64 @@
 import React, { useState } from "react";
 import CreateMeeting from "./CreateMeeting";
+import moment from "moment";
+import { postMeetingData } from "./Api/postMeeting";
+import { meetingType } from "./Types";
+import Swal from "sweetalert2";
 
 const CreateMeetingHandler = () => {
+  const now = new Date();
+  const formattedDate = now.toISOString();
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [meetingData, setMeetingData] = useState({
     programID: "",
     title: "",
-    date: null,
+    scheduleDate: null,
+    createdBy: 1,
+    meetingModeID: 1,
+    createdTime: "",
     startTime: null,
     endTime: null,
     agenda: "",
   });
 
-  const handleSchedule = () => {
-    console.log("Meeting Data:", meetingData);
-    // Here you can perform actions like sending data to the backend, etc.
+  function convertToMeetingType(input: any): meetingType {
+    return {
+      programID: input.programID,
+      title: input.title,
+      scheduleDate: input.scheduleDate.toISOString(),
+      createdBy: input.createdBy,
+      meetingModeID: input.meetingModeID,
+      createdTime: input.createdTime,
+      startTime: input.startTime.toISOString(),
+      endTime: input.endTime.toISOString(),
+      agenda: input.agenda,
+    };
+  }
+
+  const sendMeetingData = async (formatedMeetingData: meetingType) => {
+    setIsLoading(true);
+    let response = await postMeetingData(formatedMeetingData);
+    setIsLoading(false);
+    // Use SweetAlert2 to show success or error message based on response
+    if (response?.status == 201) {
+      Swal.fire("Success", "Meeting created successfully!", "success");
+    } else {
+      Swal.fire("Error", "Failed to create meeting", "error");
+    }
+  };
+
+  const handleSchedule = async () => {
+    setMeetingData((prevData) => ({
+      ...prevData,
+      ["createdTime"]: formattedDate,
+    }));
+
+    // Convert meeting data to meetingType
+    const formatedMeetingData: meetingType = convertToMeetingType(meetingData);
+
+    // Call sendMeetingData after conversion is completed
+    await sendMeetingData(formatedMeetingData);
   };
 
   const handleInputChange = (key: string, value: any) => {
