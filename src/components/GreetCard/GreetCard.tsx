bda9bfa31b4e7bce1aa85a8bb8bf546f51f58418
Aadarsh R <aadarsh.r@experionglobal.com>
2024-02-13 19:37:08 +0530

    Added Avatar

--- src/components/GreetCard/GreetCard.tsx
+++ src/components/GreetCard/GreetCard.tsx
@@ -1,19 +1,35 @@
 import React, { useEffect, useState } from "react";
-import { AspectRatio, Box, Stack, Typography } from "@mui/joy";
+import { AspectRatio, Avatar, Box, Stack, Typography } from "@mui/joy";
 import { decodeToken } from "../../apiHandler/Decoder";
 
 const GreetCard = () => {
   const [name, setName] = useState("");
-
+  const [initials, setInitials] = useState("");
+  function getInitials(name: string) {
+    // Split the name into parts
+    const parts = name.split(" ");
+    // Filter out empty strings in case there are extra spaces
+    const filteredParts = parts.filter((part) => part !== "");
+    // Map each part to its first character and join them together
+    const initials = filteredParts
+      .map((part) => part[0].toUpperCase())
+      .join("");
+    return initials;
+  }
   useEffect(() => {
     const token = sessionStorage.getItem("jwtToken");
     if (token) {
       const decoded = decodeToken(token);
       if (decoded && decoded.name) {
-        setName(decoded.name);
+        // Convert the name to uppercase before setting it
+        const upperName = decoded.name.toUpperCase();
+        setName(upperName);
+        // Set initials
+        setInitials(getInitials(upperName));
       }
     }
   }, []);
+
   return (
     <Box
       sx={{
@@ -26,13 +42,19 @@ const GreetCard = () => {
         alignItems: "center",
       }}
     >
-      <AspectRatio ratio="1" sx={{ minWidth: 130, borderRadius: "50%" }}>
-        <img
-          src="/Assets/dp.png"
-          srcSet="/Assets/dp.png"
-          loading="lazy"
-          alt=""
-        />
+      <AspectRatio
+        ratio="1"
+        sx={{ minWidth: 130, borderRadius: "50%", width: 130, height: 130 }}
+      >
+        <Avatar
+          sx={{
+            width: "100%",
+            height: "100%",
+            fontSize: "3rem",
+          }}
+        >
+          {initials}
+        </Avatar>
       </AspectRatio>
       <Stack sx={{ ml: 2 }}>
         <Typography level="title-lg">Hi,</Typography>
