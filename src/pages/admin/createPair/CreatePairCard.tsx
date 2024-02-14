2a3f874b0e32c870d5be86bf9c7476b3f90f32e6
Mehanoor Basheer <mehanoorbasheer7@gmail.com>
2024-02-14 09:46:17 +0530

    fix:create pair button disabling

--- src/pages/admin/createPair/CreatePairCard.tsx
+++ src/pages/admin/createPair/CreatePairCard.tsx
@@ -11,12 +11,12 @@ import AddIcon from "@mui/icons-material/Add";
 import { Typography } from "@mui/joy";
 import Breadcrumbs from "@mui/joy/Breadcrumbs";
 import { Box } from "@mui/system";
-// import { Link } from "react-router-dom";
 import ChevronRightRoundedIcon from "@mui/icons-material/ChevronRightRounded";
 import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
 import MentorSearchHandler from "../../../components/SearchForMentor/MentorSearchHandler";
 import MenteeSearchHandler from "../../../components/SearchForMentee/MenteeSearchHandler";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 interface CreateProgramProps {
   onSubmit: () => void;
@@ -27,11 +27,33 @@ const CreatePairCard = ({ onSubmit, onChange }: CreateProgramProps) => {
   const [mentorID, setMentorID] = useState(0);
   const [menteeID, setMenteeID] = useState(0);
   const [startDate, setStartDate] = React.useState(null);
+  const [programName, setProgramName] = React.useState("");
+  const [endDate, setEndDate] = React.useState(null);
+  const [isButtonDisabled, setIsButtonDisabled] = React.useState(true);
+  const history = useNavigate(); // for navigantion
 
+  // Effect to update button state based on field completion as false
+  useEffect(() => {
+    setIsButtonDisabled(
+      mentorID === 0 ||
+        menteeID === 0 ||
+        !startDate ||
+        !programName ||
+        programName.trim() === "" ||
+        !endDate // Disable button if endDate is not set
+    );
+  }, [mentorID, menteeID, startDate, programName, endDate]);
+
+  //to pass to endDate setting component
   const handleStartDateChange = (date: React.SetStateAction<null>) => {
-    setStartDate(date); //set the start date to the date that got from setStartDate component
-    console.log(startDate);
+    setStartDate(date);
   };
+
+  const handleProgramNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setProgramName(value);
+  };
+
   useEffect(() => {
     onChange("mentorID", mentorID);
   }, [mentorID]);
@@ -41,6 +63,18 @@ const CreatePairCard = ({ onSubmit, onChange }: CreateProgramProps) => {
   useEffect(() => {
     onChange("startDate", startDate);
   }, [startDate]);
+  useEffect(() => {
+    onChange("programName", programName);
+  }, [programName]);
+  useEffect(() => {
+    onChange("endDate", endDate);
+  }, [endDate]);
+
+  // Handle form submission
+  const handleSubmit = () => {
+    onSubmit(); //to post form
+    history("/admin/pairs"); // Navigate to pairs page upon submission
+  };
 
   return (
     <React.Fragment>
@@ -92,9 +126,9 @@ const CreatePairCard = ({ onSubmit, onChange }: CreateProgramProps) => {
             }}
           >
             <Input
-              placeholder="                                             Program Name.."
+              placeholder="Program Name.."
               sx={{ border: "none", bgcolor: "transparent" }}
-              onChange={(e) => onChange("programName", e.target.value)}
+              onChange={handleProgramNameChange}
             />
           </Card>
         </Grid>
@@ -132,9 +166,9 @@ const CreatePairCard = ({ onSubmit, onChange }: CreateProgramProps) => {
               <CardContent>
                 <SetEndDate
                   startDate={startDate}
-                  onChange={(date: React.SetStateAction<null>) =>
-                    onChange("endDate", date)
-                  }
+                  onChange={(date: React.SetStateAction<null>) => {
+                    setEndDate(date);
+                  }}
                 />
                 {/* pass the starting date to setEndDate component */}
               </CardContent>
@@ -143,7 +177,10 @@ const CreatePairCard = ({ onSubmit, onChange }: CreateProgramProps) => {
         </Grid>
         <Grid xs={12}>
           <Grid container justifyContent="center">
-            <Button onClick={onSubmit}>Create Pair</Button>
+            <Button onClick={handleSubmit} disabled={isButtonDisabled}>
+              <AddIcon />
+              Create Pair
+            </Button>
           </Grid>
         </Grid>
       </Grid>
