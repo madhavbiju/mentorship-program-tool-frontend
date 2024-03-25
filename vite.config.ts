cd6762f2139784498731f3ec16d0448bfd118fef
madhav-biju <madhav.b@experionglobal.com>
2024-03-25 11:41:41 +0530

    Update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    target: 'esnext',
+    platform: 'node'
+  },
   server: {
     port: 3000,
   },
